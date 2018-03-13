2ab33404f59bb42402b285774933aa1de320a2d3
Dovile <dovile.makselyte@gmail.com>
2018-03-13 23:00:56 +0200

    item card ui in home page

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-card class="card" :body-style="{ padding: '0px' }">
+    <img :src="item.img" class="image">
+    <div class="itemCardPrice">
+      <span>{{item.price}} €</span>
+    </div>
+    <div style="padding: 3%;">
+      <el-tooltip class="module line-clamp" :content="item.name">
+        <span>{{item.name}}</span>
+      </el-tooltip>
+      <!-- Uzkomentuotas mygtukas, jei reiks 'Add to cart' is home page mygtuko, tai atkomentuosime. -->
+      <!-- <div class="bottom">
+        <el-button class="itemCardButton" type="primary" icon="el-icon-plus" size="medium">
+          Add to cart
+        </el-button>
+      </div> -->
+      <div style="border-bottom: 1px solid;"/>
+    </div>
+    <!-- Cia bus ideti pirmi du pasitaike imet'o pozymiai. 
+    Kadangi pozymiu gali ir nebuti / buti vienas / buti du, tai darant ideti v-if salyga.
+    Card'o dydis neturetu kisti => line-height: 1.2em; height: 2.4em;-->
+    <div class="itemCardAttributes">
+      <div>
+        <span>Spalva: </span>
+        <span>ruda</span>
+      </div>
+      <div>
+        <span>Dydis: </span>
+        <span>gigantiškas</span>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+
+<style>
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+  .image {
+    width: 100%;
+    display: block;
+  }
+  .itemCardAttributes{
+    text-align: left;
+    font-size: 10pt;
+    margin-left: 5%;
+    margin-bottom: 3%;
+    color: grey;
+    line-height: 1.2em;
+    height: 2.4em;
+  }
+  .itemCardButton{
+    width: 50%;
+    padding: 2%;
+  }
+  .module {
+    overflow: hidden;
+    line-height: 1.2em;
+    height: 2.4em;
+    text-align: center;
+    font-size: 12pt;
+    margin: 2%;
+  }
+  .line-clamp {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;  
+  }
+  .itemCardPrice{
+    color:midnightblue;
+    font-weight: bold;
+    font-size: 12pt;
+    text-align: left;
+    padding-left: 10%;
+    padding-top: 2%;
+  }
+</style>
+
+<script>
+export default {
+  props: [ 'item' ]
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,33 @@
 <template>
-<div class="eshop-container-wrapper">
-  <div class="container">
-    <b-container >
-    <b-row >
-      <b-col class="col-wrapper" v-for="item in goods" :key="item.name">
-        <div class="card" style="width: 18rem;">
-        <img class="card-img-top" :src="item.img" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title" v-text="item.name"></h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Add to cart</a>
-            </div>
-        </div>
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
-</div>
-
+  <el-row v-if="items" >
+    <el-col class="homePageItemCard" v-for="item in items" :key="item.SKU" :xs="12" :sm="8" :md="6" :lg="4">
+      <ItemCard  :item="item"></ItemCard>
+    </el-col>
+  </el-row>
 </template>
+
 <script>
+import ItemCard from './../components/ItemCard'
 export default {
-    data () {
-        return {
-            goods: []
-        }
-    },
-    mounted () {
-      this.axios.get('api/items').then(response => {
-        this.goods = response.data
-      }).catch(error => {
-        console.log(error)
-      })
-    }
+  data () {
+      return {
+          items: []
+      }
+  },
+  components: {
+    ItemCard
+  },
+  mounted () {
+    this.axios.get('api/items').then(response => {
+      this.items = response.data
+    }).catch(error => {
+      console.log(error)
+    })
+  }
 }
 </script>
 <style>
-    .eshop-container-wrapper {
-        margin-top: 15px;
-    }
-
-    .col-wrapper {
-        margin-top: 25px;
-    }
-
+  .homePageItemCard{
+    padding: 10px;
+  }
 </style>
