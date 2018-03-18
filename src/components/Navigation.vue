3fb560d4a6b2b9272c44d0efa9211c31348462d3
Greta Mameniskyte <gretuka27@gmail.com>
2018-03-18 17:35:20 +0100

    Small changes to navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -26,36 +26,6 @@
         {{(goodsInCart)}}
       </el-menu-item>
     </el-menu>
-    <el-menu mode="horizontal" :router="true" :default-active="activeIndex"
-     background-color="#545c64" text-color="#fff" active-text-color="#0080FF"
-     v-if="smallScreen">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-more-outline"></i>
-        </template>
-        <el-menu-item index="1-1" route="/home">
-          <template slot="title">
-            <span>Goal Diggers</span>
-          </template>
-        </el-menu-item>
-        <el-menu-item index="2-1" v-for="category in categories">
-          {{category.name}}
-        </el-menu-item>
-      </el-submenu>
-      <el-menu-item class="gd_right" index="1-8" v-if="!isAuthorised" route="/register">Sign up</el-menu-item>
-      <el-menu-item class="gd_right" index="1-7" v-if="!isAuthorised" route="/login">Log in</el-menu-item>
-      <el-submenu class="gd_right" index="1-6" v-if="isAuthorised">
-        <template slot="title">
-          <i class="el-icon-menu"></i>
-        </template>
-        <el-menu-item index="1-6-1" route="/user/profile">My profile</el-menu-item>
-        <el-menu-item index="1-6-2" route="#">Sign out</el-menu-item>
-      </el-submenu>
-      <el-menu-item class="gd_right" index="1-5" route="#">
-        <i class="el-icon-goods"></i>
-        {{(goodsInCart)}}
-      </el-menu-item>
-    </el-menu>
   </div>
 </template>
 
@@ -67,8 +37,6 @@ export default {
       goodsInCart: 0,
       isAuthorised: false,
       displayMode: 'horizontal',
-      smallScreen: '',
-      fullWidth: document.documentElement.clientWidth,
       categories: [
         {
           name: 'Category 0',
@@ -90,24 +58,6 @@ export default {
         }
       ]
     }
-  },
-  beforeDestroy: function () {
-    window.removeEventListener('resize', this.handleResize)
-  },
-  methods: {
-    handleResize (event) {
-      this.fullWidth = document.documentElement.clientWidth
-      console.log(this.fullWidth)
-      console.log('Window.screen.width' + window.screen.width)
-      if (this.fullWidth < 755 || window.screen.width < 755) {
-        this.smallScreen = true
-      } else {
-        this.smallScreen = false
-      }
-    }
-  },
-  mounted () {
-    window.addEventListener('resize', this.handleResize)
   }
 }
 </script>
