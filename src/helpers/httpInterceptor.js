6448d23eae8fa1f769b82c3c9a0e256b57c5fb92
Saulius Skliutas <saulius.skliutas@gmail.com>
2018-03-26 22:25:29 +0300

    Implement anti forgery validation

--- src/helpers/httpInterceptor.js
+++ src/helpers/httpInterceptor.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-import store from '@/store'
+import Vue from 'vue'
 
 export default function execute () {
     axios.interceptors.request.use(function (config) {
-        const token = store.getters.token
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
+        var xsrfToken = Vue.cookie.get('CSRF-TOKEN')
+        if (xsrfToken) {
+            config.headers['X-CSRF-TOKEN'] = xsrfToken
         }
         return config
     }, function (err) {
