7494f9e2fe022421ce6a3f19469d7d7501d2f6ea
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-03-26 14:23:35 +0300

    fixing deploy.cmd file

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -6,6 +6,7 @@ for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
 xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET% /s /i
 xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*
+xcopy /d %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config*
 
 call npm install --only=production
 echo Deployed
