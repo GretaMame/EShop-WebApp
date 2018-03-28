8da4bfbdb12590a954d7863c34047131d47b840e
Saulius Skliutas <saulius.skliutas@gmail.com>
2018-03-28 23:54:00 +0300

    merge cleanup

--- src/helpers/httpInterceptor.js
+++ src/helpers/httpInterceptor.js
@@ -1,20 +1,8 @@
 import axios from 'axios'
-import Vue from 'vue'
 import Store from '@/store'
 import Router from '@/router'
 
 export default function execute () {
-    axios.interceptors.request.use(function (config) {
-        var xsrfToken = Vue.cookie.get('CSRF-TOKEN')
-        if (xsrfToken) {
-            config.headers['X-CSRF-TOKEN'] = xsrfToken
-        }
-        return config
-    }, function (err) {
-        console.log(err)
-        return Promise.reject(err)
-    })
-
     axios.interceptors.response.use(function (response) {
         return response
     }, function (err) {
