ba0b8fd15a17f726802f89277ee201a433698a2d
Saulius Skliutas <saulius.skliutas@gmail.com>
2018-03-30 01:49:36 +0300

    Fix wrong commit

--- src/main.js
+++ src/main.js
@@ -21,9 +21,9 @@ import VueCookie from 'vue-cookie'
 Vue.component('icon', Icon)
 Vue.use(VueAxios, axios)
 Vue.use(VueCookie)
-Vue.axios.defaults.baseURL = 'http://localhost:60014/api/'
+// Vue.axios.defaults.baseURL = 'http://localhost:60014/api/'
 // Vue.axios.defaults.baseURL = 'http://localhost:5000/api/'
-// Vue.axios.defaults.baseURL = 'https://eshop-qa-api.azurewebsites.net/api/'
+Vue.axios.defaults.baseURL = 'https://eshop-qa-api.azurewebsites.net/api/'
 Vue.axios.defaults.withCredentials = true
 Vue.axios.defaults.xsrfCookieName = 'CSRF-TOKEN'
 Vue.axios.defaults.xsrfHeaderName = 'X-CSRF-TOKEN'
