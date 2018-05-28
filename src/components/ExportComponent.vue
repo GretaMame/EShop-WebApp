e984d9db99fac8127a5abd7dcea61494bc1451f4
Greta Mameniskyte <gretuka27@gmail.com>
2018-05-29 00:14:41 +0300

    prevent window from closing while performing import

--- src/components/ExportComponent.vue
+++ src/components/ExportComponent.vue
@@ -3,7 +3,6 @@
     <div>
       <el-cascader @blur="left" :disabled="exportStarted" v-model="exportCategoryObj" placeholder="Select Category/Subcategory"
         class="select-class" v-loading="categoriesLoading" :options="options" change-on-select></el-cascader>
-      </el-select>
       <div v-if="noCategorySelected" class="gd-err-msg">
         Select category or subcategory first.
       </div>
@@ -214,9 +213,6 @@
   .disabled:hover {
     cursor: wait;
   }
-
-  .select-class {}
-
   ul {
     list-style-type: none;
   }
